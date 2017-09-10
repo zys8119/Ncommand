763858353367057b49c3b8c1d58f6ba3e263a4c8
zys8119 <770959294@qq.com>
2017-09-10 21:53:34 +0800

    Ncommand项目搭建

---
+++ test.js
@@ -0,0 +1,13 @@
+
+// new require("./command")()
+//     .Options("b","a   ",1,{},465,["303"])
+//     .Options("b","a   ",1,{},465,["33"])
+//     .console.rad("asdas")
+var command = require("./command");
+new command()
+    .Commands({
+        txt:["sdsad","adsadas","asdasdas"],
+        callback:123
+    })
+    .Commands()
+    .Commands()
