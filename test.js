40fd76a618862194e06f919cbbec4fdc553ed858
zys8119 <770959294@qq.com>
2017-09-11 12:24:01 +0800

    Ncommand项目搭建

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ new command()
         log:["aa","asdasdas","<a>",{},[1],"sdfsdf...red('sdfsd')","...info('12')8778","left...info()right",".red('A')","..red('B')","weqw...info('2')wqe..info()...red('asda')","testTxt","...abc('155')",`asa`],
     })
     .Commands({
-        log:["left<...red('标签')>right","脚本"],
+        log:["left<...red('标签')>right","脚本1"],
     })
     .Options()
